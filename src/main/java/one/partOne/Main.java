62efd78a44f7eeafa72365aed28664ca76901d96
Maxim Belyak <belyak.maxxim@gmail.com>
2023-10-10 13:33:12 +0300

    Initial commit

---
+++ src/main/java/one/partOne/Main.java
@@ -0,0 +1,134 @@
+package one.partOne;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.SplittableRandom;
+
+public class Main {
+    static Random random = new Random();
+    static SplittableRandom splittableRandom = new SplittableRandom();
+
+    public static void main(String[] args) {
+        //1.
+        int[] arrayOne = new int[10];
+
+        //2.
+        int[] arrayTwo = new int[]{1, 2, 3, 4, 5};
+
+        //3.
+        int size = 10;
+        int[] arrayThree = new int[size];
+        for (int i = 0; i < size; i++) {
+            arrayThree[i] = random.nextInt(-10, 11);
+        }
+
+        //4.
+        double[] arrayFour = splittableRandom.doubles(size, -10, 11).toArray();
+
+        //5.
+        int[] arrayFiveOne = splittableRandom.ints(size, -10, 11).toArray();
+
+        int newSize = size + 5;
+        int[] arrayFiveTwo = new int[newSize];
+
+        for (int i = 0; i < arrayFiveOne.length; i++) {
+            arrayFiveTwo[i] = arrayFiveOne[i];
+        }
+
+        //6.
+        int[] arraySix = splittableRandom.ints(12, -15, 16).toArray();
+
+        int maxNumber = arraySix[0];
+        int indexOfMaxNumber = 0;
+
+        for (int i = 0; i < arraySix.length; i++) {
+            if (arraySix[i] >= maxNumber) {
+                maxNumber = arraySix[i];
+                indexOfMaxNumber = i;
+            }
+        }
+
+        //7.
+        int[] arraySeven = splittableRandom.ints(8, 1, 11).toArray();
+
+        for (int i = 0; i < arraySeven.length; i++) {
+            if (i % 2 == 1) {
+                arraySeven[i] = 0;
+            }
+        }
+
+        //8.
+        int[] arrayEight = splittableRandom.ints(4, 10, 100).toArray();
+
+        boolean isIncreasing = true;
+        for (int i = 1; i < arrayEight.length; i++) {
+            if (arrayEight[i] < arrayEight[i - 1]) {
+                isIncreasing = false;
+            }
+        }
+
+        if (isIncreasing) {
+//            System.out.println("массив является строго возрастающей последовательностью");
+        } else {
+//            System.out.println("массив не является строго возрастающей последовательностью");
+        }
+
+        //9.
+        int arraysSize = 5;
+        int[] arrayNineOne = splittableRandom.ints(arraysSize, 0, 6).toArray();
+//        System.out.println("First massive : " + Arrays.toString(arrayNineOne));
+
+        int[] arrayNineTwo = splittableRandom.ints(arraysSize, 0, 6).toArray();
+//        System.out.println("Second massive : " + Arrays.toString(arrayNineTwo));
+
+        double firstAvg = calculateAvg(arrayNineOne);
+        double secondAvg = calculateAvg(arrayNineTwo);
+
+        if (firstAvg > secondAvg) {
+//            System.out.println("firstAvg > secondAvg");
+        } else if (firstAvg < secondAvg) {
+//            System.out.println("firstAvg < secondAvg");
+        } else {
+//            System.out.println("firstAvg = secondAvg");
+        }
+
+        //10.
+        int[] arrayTenOne = splittableRandom.ints(50, 0, 1001).toArray();
+
+        int count = 0;
+        for (int num : arrayTenOne) {
+            if (num % 2 == 0) {
+                count++;
+            }
+        }
+
+        int[] arrayTenTwo = new int[count];
+        int index = 0;
+        for (int i = 0; i < arrayTenOne.length; i++) {
+            if (arrayTenOne[i] % 2 == 0) {
+                arrayTenTwo[index] = arrayTenOne[i];
+                index++;
+            }
+        }
+
+        //11.
+        int[] a = new int[]{1, 2, 3, 4, 5};
+        int[] b = new int[]{-1, -2, -3, -4, -5};
+
+        int[] c = new int[a.length + b.length];
+
+        System.arraycopy(a, 0, c, 0, a.length);
+        System.arraycopy(b, 0, c, a.length, b.length);
+
+        System.out.println(Arrays.toString(c));
+    }
+
+    //9.
+    public static double calculateAvg(int[] array) {
+        double sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum / array.length;
+    }
+}
---
+++ src/main/java/one/partThree/Main.java
@@ -0,0 +1,176 @@
+package one.partThree;
+
+import javax.swing.*;
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+import java.util.Arrays;
+import java.util.SplittableRandom;
+
+public class Main {
+    static SplittableRandom splittableRandom = new SplittableRandom();
+
+    public static void main(String[] args) {
+        //1-5.
+//        int[] array = splittableRandom.ints(10, -100, 100).toArray();
+//        System.out.println(Arrays.toString(array));
+//
+//        System.out.printf("Avg : %2.2f\n", calculateAvg(array));
+//
+//        System.out.printf("Max number : %d\n", findMaxNumber(array));
+//        System.out.printf("Min number : %d\n", findMinNumber(array));
+//
+//        findDuplicate(array);
+//
+//        findSecondMaxNumber(array);
+//
+//        findSecondMinNumber(array);
+
+        //6.
+//        int[] array2 = splittableRandom.ints(5, 1, 6).toArray();
+//        System.out.println(Arrays.toString(array2));
+//
+//        int[] array3 = splittableRandom.ints(5, 1, 6).toArray();
+//        System.out.println(Arrays.toString(array3));
+//
+//        findDuplicateFromTwoMassive(array2, array3);
+
+        //7.
+        int[] array4 = splittableRandom.ints(5, 1, 6).toArray();
+        System.out.println(Arrays.toString(array4));
+
+        int[] array5 = splittableRandom.ints(5, 1, 6).toArray();
+        System.out.println(Arrays.toString(array5));
+
+        findUniqueNumberFromTwoMassive(array4, array5);
+    }
+
+    //1.
+    public static double calculateAvg(int[] array) {
+        double sum = 0;
+
+        for (int num : array) {
+            sum += num;
+        }
+
+        return sum / array.length;
+    }
+
+    //2.
+    public static int findMaxNumber(int[] array) {
+        int max = array[0];
+
+        for (int num : array) {
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max;
+    }
+
+    //2.
+    public static int findMinNumber(int[] array) {
+        int min = array[0];
+
+        for (int num : array) {
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        return min;
+    }
+
+    //3.
+    public static void findDuplicate(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i] == array[j] && i != j) {
+                    System.out.println(array[i] + " is duplicate.");
+                }
+            }
+        }
+    }
+
+    //4.
+    public static void findSecondMaxNumber(int[] array) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for (int num : array) {
+            if (num > max1) {
+                max2 = max1;
+                max1 = num;
+            } else if (num > max2 && num != max1) {
+                max2 = num;
+            }
+        }
+
+        if (max2 != Integer.MIN_VALUE) {
+            System.out.println("Second max number : " + max2);
+        } else {
+            System.out.println("Massive don't have second max number!");
+        }
+    }
+
+    //5.
+    public static void findSecondMinNumber(int[] array) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int num : array) {
+            if (num < min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num < min2 && num != min1) {
+                min2 = num;
+            }
+        }
+
+        if (min2 != Integer.MAX_VALUE) {
+            System.out.println("Second min number : " + min2);
+        } else {
+            System.out.println("Massive don't have second min number!");
+        }
+    }
+
+    public static void findDuplicateFromTwoMassive(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i] == b[j]) {
+                    System.out.println(a[i] + " is duplicate!");
+                }
+            }
+        }
+    }
+    public static void findUniqueNumberFromTwoMassive(int[] a, int[] b) {
+        for (int numA : a) {
+            boolean isUnique = true;
+
+            for (int numB : b) {
+                if (numA == numB) {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique) {
+                System.out.println(numA + " є унікальним!");
+            }
+
+        }
+        for (int numB : b) {
+            boolean isUnique = true;
+
+            for (int numA : a) {
+                if (numB == numA) {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique) {
+                System.out.println(numB + " є унікальним!");
+            }
+        }
+    }
+}
