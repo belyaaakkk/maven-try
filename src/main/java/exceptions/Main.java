b2d213f51ac2d562dfbc98cbfc337162c4cce660
Maxim Belyak <belyak.maxxim@gmail.com>
2023-10-24 15:57:41 +0300

    Exceptions practice task 2 was done

--- src/main/java/exceptions/Main.java
+++ src/main/java/exceptions/Main.java
@@ -4,20 +4,48 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 0, b = 0, c = 0;
-        Random random = new Random();
-
-        for (int i = 0; i < 32000; i++) {
-            try {
-                b = random.nextInt();
-                c = random.nextInt();
-                a = 12345 / (b / c);
-            } catch (ArithmeticException e){
-                System.out.println("Исключение : " + e);
-                a = 0;
-            }
-            System.out.println("a: " + a);
-
+//1       int a = 0, b = 0, c = 0;
+//        Random random = new Random();
+//
+//        for (int i = 0; i < 32000; i++) {
+//            try {
+//                b = random.nextInt();
+//                c = random.nextInt();
+//                a = 12345 / (b / c);
+//            } catch (ArithmeticException e){
+//                System.out.println("Исключение : " + e);
+//                a = 0;
+//            }
+//            System.out.println("a: " + a);
+//
+//        }
+//2        try {
+//            int a = args.length;
+//
+//            int b = 42 / a;
+//            System.out.println("a = " + a);
+//            try {
+//                if (a == 1) {
+//                    a = a / (a - a);
+//                }
+//                if (a == 2) {
+//                    int[] c = {1};
+//                    c[42] = 99;
+//                }
+//            } catch (ArrayIndexOutOfBoundsException e) {
+//                System.out.println("Выход за допустимые пределы индекса в масиве : " + e);
+//            }
+//        } catch (ArithmeticException e) {
+//            System.out.println("Деление на ноль: " + e);
+//        }
+        try {
+            throwOne();
+        } catch (IllegalAccessException e){
+            System.out.println("Перехвачено " + e);
         }
+    }
+    static void throwOne() throws IllegalAccessException {
+        System.out.println("Внутри throwOne().");
+        throw new IllegalAccessException("demo");
     }
 }
